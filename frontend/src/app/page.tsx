9df6060dd3c2be6e2639cb5b6b5da1a80be471ae
rizal-summitglobal <rizal@summitglobal.id>
2025-04-03 23:07:31 +0700

    start development

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,34 +1,8 @@
+import style from './page.module.scss';
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="container container__home container-fluid flex items-center w-full h-screen justify-center">
-      <div className="content-1">
-        <div className="text-container w-[48vw]">
-          <div className="title text-[2.778vw] font-bold">
-            Unlock Your Potential <br /> with RzDev
-          </div>
-          <div className="description text-[1.111vw] text-gray-400">
-          Welcome to Byway, where learning knows no bounds. We believe that <br /> education is the key to personal and professional growth, and we're here to <br />guide you on your journey to success. Whether you're a student, professional, <br /> or lifelong learner, our cutting-edge Learning Management System is <br /> designed to elevate your learning experience.
-          </div>
-        </div>
-        <div className="w-[17vw] bg-[#2563EB] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex justify-center">
-        <button>
-          Start your instructor Journey 
-        </button>
-        </div>
-      </div>
-      <div className="content-2">
-        <div className="swiper">
-          <div className="swiper-container">
-            <Image 
-              src={"/images/set.jpg"} 
-              alt="Banner Hero" 
-              width={600}
-              height={400}/>
-          </div>
-        </div>
-      </div>
-    </div>
+    <h1>Hello World!</h1>
   );
 }
