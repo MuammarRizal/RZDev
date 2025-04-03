4fd37862c32dcabc9d9ac5bbbe00df9b2e35fef9
rizal-summitglobal <rizal@summitglobal.id>
2025-04-03 21:53:50 +0700

    update background default to white

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,27 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div className="container__home container-fluid flex items-center w-full h-screen justify-center">
+      <div className="content-1">
+        <div className="text-container w-[48vw]">
+          <div className="title text-[2.778vw] font-bold">
+            Unlock Your Potential
+          </div>
+          <div className="description text-[1.111vw] text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ipsam harum fuga repudiandae sequi! Labore eligendi necessitatibus facere similique, explicabo voluptate in sunt consequatur repudiandae aliquid ipsa iure rerum cum quam maiores mollitia incidunt? Minima saepe numquam, necessitatibus tenetur possimus quia provident voluptate architecto facilis officiis similique tempore rerum incidunt!
+          </div>
+        </div>
+        <div className="btn btn-primary py-2 px-3 bg-blue-800 w-60">
+          <button>Start your intructor journey</button>
+        </div>
+      </div>
+      <div className="content-2">
+        <div className="swiper">
+          <div className="swiper-container">
+
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
